2230d97c63cfc60329c95918be2c76a4a006cc37
Gleb Skripnikov <Gleb Skripnikov>
2016-08-08 15:39:16 +0300

    Readme and config changed

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # BooksTest
+
+1. Install Node.js v6.3.1 https://nodejs.org/en/download/current/
+2. Install Gulp ($ npm i gulp -g)
+3. Instal node_modules ($ npm i)
+4. Run Gulp ($ gulp)
+5. Create DB book_test (book_test.sql)
+5. Run server ($ npm start)
--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var historyApiFallback = require('connect-history-api-fallback');
 var sass = require('gulp-sass');
+var sysBuilder = require('systemjs-builder');
 
-gulp.task('watch', ['sass'], function () {
+gulp.task('watch', ['sass'], function() {
 
     browserSync.init({
         proxy: 'localhost:3001',
@@ -16,28 +17,43 @@ gulp.task('watch', ['sass'], function () {
     gulp.watch("./public/app/**/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', function() {
     return gulp.src("./public/app/**/*.sass")
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest("./public/app/"));
 });
 
-gulp.task('libs', function () {
+gulp.task('libs', function() {
     gulp.src([
-        'core-js/client/shim.min.js',
-        'core-js/client/shim.min.js.map',
-        'zone.js/dist/zone.js',
-        'reflect-metadata/Reflect.js',
-        'reflect-metadata/Reflect.js.map',
-        'systemjs/dist/system.src.js',
-        'rxjs/bundles/Rx.min.js',
-        'rxjs/bundles/Rx.min.js.map',
-        '@angular/**/*.js',
-        '@angular/**/*.js.map',
-        '@angular/**/index.js',
-        '@angular/**/index.js.map'
-    ], { cwd: "node_modules/**" })
+            'core-js/client/shim.min.js',
+            'core-js/client/shim.min.js.map',
+            'zone.js/dist/zone.js',
+            'reflect-metadata/Reflect.js',
+            'reflect-metadata/Reflect.js.map',
+            'systemjs/dist/system.src.js',
+            'rxjs/bundles/Rx.min.js',
+            'rxjs/bundles/Rx.min.js.map',
+            'rxjs/**/*.js',
+            'rxjs/**/*.js.map',
+            '@angular/**/*.js',
+            '@angular/**/*.js.map',
+            '@angular/**/index.js',
+            '@angular/**/index.js.map'
+        ], {
+            cwd: "node_modules/**"
+        })
         .pipe(gulp.dest("./public/app/libs"));
+});
+
+gulp.task('bundle:js', function() {
+    var builder = new sysBuilder('public', './public/systemjs.config.js');
+    return builder.buildStatic('app', 'public/dist/js/app.min.js')
+        .then(function() {
+            return del(['public/dist/js/**/*', '!public/dist/js/app.min.js']);
+        })
+        .catch(function(err) {
+            console.error('>>> [systemjs-builder] Bundling failed'.bold.green, err);
+        });
 });
 
 gulp.task('default', ['libs', 'sass']);
